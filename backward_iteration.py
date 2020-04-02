b99448de5e0dece26f70bfa1226cb5475b376f63
ylee0908 <57956815+ylee0908@users.noreply.github.com>
2020-04-02 08:09:19 +0100

    Create backward_iteration.py

---
+++ backward_iteration.py
@@ -0,0 +1,21 @@
+A= [5,6,7,8]
+
+for i in range(len(A), 0, -1):
+	print (i) #4,3,2,1,
+ 
+for i in range(len(A)-1, -1, -1)
+  print (A(i)) # 8,7,6,5
+  
+for i in reversed(range(1, len(A)+1)):
+	print (i)  # 4,3,2,1,
+  
+for i in reversed(range(0, len(A)):
+  print (A(i))  #8,7,6,5
+
+
+for i in range(len(A)-1, -1, -1):
+	print ({i: A[i]}) 
+
+for i in reversed(range(0, len(A))):
+	print ({i: A[i]})
+
