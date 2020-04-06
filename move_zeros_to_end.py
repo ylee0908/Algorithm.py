62c3ac35830c4ab61d9420d0851f027178e92d75
ylee0908 <57956815+ylee0908@users.noreply.github.com>
2020-04-06 15:40:28 +0100

    Update move_zeros_to_end.py

--- move_zeros_to_end.py
+++ move_zeros_to_end.py
@@ -11,13 +11,25 @@ Minimize the total number of operations.
 
 class Solution(object):
     def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        count = 0
+        
+        pos = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[count], nums[i] = nums[i], nums[count]
+                nums[pos], nums[i] = nums[i], nums[pos]
                 count += 1
+
+# two pointers method
+def movezero(nums):
+    slow = fast = 0
+    while fast < len(nums):
+        if nums[slow] == 0 and nums[fast] != 0:
+            nums[slow], nums[fast] = nums[fast], nums[slow]
+
+        # wait while we find a non-zero element to
+        # switch with you
+        if nums[slow] != 0:
+            slow += 1
+
+        # keep going
+        fast += 1
                 
