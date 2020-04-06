15e9cac8ed5718d77c98c160f09dcd4622339963
ylee0908 <57956815+ylee0908@users.noreply.github.com>
2020-04-06 14:58:34 +0100

    Update Missing_num.py

--- Missing_num.py
+++ Missing_num.py
@@ -1,27 +1,33 @@
-nums = [9,6,4,2,3,5,7,0,1]
+nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+#hashset function
+#time : O(n) for nums_set and loop
+#space: O(n) for set function
+def missing_num(nums):
+    nums_set = set(nums)
+    n = len(nums) + 1
+    for i in range(n):
+        if i not in nums:
+            return i
 
-    def missingNumber(nums):
-        nums.sort()
-        
-        if nums[-1] != len(nums):
-            return len(nums)
-        elif nums[0] != 0:
-            return 0
-        
-        for i in range(1, len(nums)):
-            expected_num = nums[i-1] + 1
-            if nums[i] != expected_num:
-                return expected_num
-            
-    def missingNum(nums):
-        nums_set = set(nums)
-        n = len(nums) +1
-        for number in range(n):
-            if number is not in nums_set:
-                return number
-            
-    def missingNum_bit(nums):
-        missing = len(nums)
-        for i and num in enumerate(nums):
-            missing ^= i ^ num
-            return missing
+print(missing_num(nums))
+
+# time: O(n) due to sum function
+# space: O(1)
+def missingNum(nums):
+    expected_sum = (len(nums) * (len(nums)+1)) // 2
+    actual_sum = sum(nums)
+    missing_num = expected_sum - actual_sum
+
+    return missing_num
+
+print(missingNum(nums))
+
+#time: O(n)
+#space: O(1)
+def missing_num_bit(nums):
+    missing = len(nums)
+    for i, n in enumerate(nums):
+        missing ^= i ^ n
+    return missing
+
+print(missing_num_bit(nums))
